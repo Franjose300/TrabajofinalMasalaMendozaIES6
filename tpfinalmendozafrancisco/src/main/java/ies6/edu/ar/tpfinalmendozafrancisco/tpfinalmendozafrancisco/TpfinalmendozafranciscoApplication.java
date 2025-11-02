0b7e7114054c0948bef1dd66cdd18f4e899c9e61
Francisco Mendoza <franciscojosemendoza07@gmail.com>
2025-11-01 22:31:24 -0300

    Inicializacion del proyecto final Mendoza Francisco

---
+++ tpfinalmendozafrancisco/src/main/java/ies6/edu/ar/tpfinalmendozafrancisco/tpfinalmendozafrancisco/TpfinalmendozafranciscoApplication.java
@@ -0,0 +1,13 @@
+package ies6.edu.ar.tpfinalmendozafrancisco.tpfinalmendozafrancisco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpfinalmendozafranciscoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpfinalmendozafranciscoApplication.class, args);
+	}
+
+}
---
+++ tpfinalmendozafrancisco/src/test/java/ies6/edu/ar/tpfinalmendozafrancisco/tpfinalmendozafrancisco/TpfinalmendozafranciscoApplicationTests.java
@@ -0,0 +1,13 @@
+package ies6.edu.ar.tpfinalmendozafrancisco.tpfinalmendozafrancisco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpfinalmendozafranciscoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
